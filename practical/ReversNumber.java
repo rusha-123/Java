2e81b5660af51154985c563a2339d4b35b6e7a03
rusha-123 <107669514+rusha-123@users.noreply.github.com>
2022-06-21 12:04:36 +0530

    Add files via upload

---
+++ practical/ReversNumber.java
@@ -0,0 +1,13 @@
+package SwitchX;
+
+public class ReversNumber {
+
+	public static void main(String[] args) 
+	 {
+		for(int i=1, j=5; i<=5; i++, j--) 
+		{
+		System.out.println(i +" "+j);
+
+	}
+	 }
+}
